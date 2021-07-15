2425f8308724192662975e5651dea94135c0b3b0
vickoman <vicmandlagasca@gmail.com>
2021-07-15 17:52:09 -0500

    separate-component Separate Fruit Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import FruitCard from './components/FruitCard'
 import "./styles.css";
 
 // Data
@@ -9,65 +10,9 @@ const data = [
   { name: "Melon", price: 5.5 },
 ];
 
-/**
- * @class FruitCard
- * @extends React.Component
- * @property {string} name
- * @property {number} price
- **/
-class FruitCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cantidad: 0
-    };
-
-    const METHODS = [
-      'agregar',
-      'restart',
-      'limpiar'
-    ];
-
-    METHODS.forEach(method => {
-      this[method] = this[method].bind(this);
-    });
-  }
-
-  agregar() {
-    this.setState({cantidad: this.state.cantidad + 1});
-  }
-
-  restart() {
-    this.setState({cantidad: this.state.cantidad - 1});
-  }
-
-  limpiar() {
-    this.setState({cantidad: 0});
-  }
-
-  render() {
-    const { name, price } = this.props;
-    return (
-      <div className="card">
-        <div className="card-content">
-          <div className="card-title">{name}</div>
-          <div className="card-subtitle">{price}</div>
-          <div className="card-action">Cantidad: {this.state.cantidad}</div>
-          <button
-            onClick={this.agregar}>+</button>
-          <button onClick={this.restart}>-</button>
-          <button onClick={this.limpiar}>Limpiar</button>
-          <hr />
-        </div>
-      </div>
-    );
-  }
-}
-
-export default function App() {
+const App = () => {
   return (
     <div className="App">
-      {/* <CardFruit fruits={data} /> */}
       <FruitCard name="Apple" price={2.5} />
       <FruitCard name="Orange" price={3.5} />
       <FruitCard name="Banana" price={1.5} />
@@ -75,3 +20,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
